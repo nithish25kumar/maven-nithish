cb46592befa5cfe544275d9127d3c92ba1a2e04c
Batch1 <Batch1@VITUNIVERSITY.LOCAL>
2026-04-06 10:27:10 +0530

    maven folder

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    App calculator = new App();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1, calculator.subtract(3, 2));
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2, calculator.divide(4, 2));
+    }
+}
